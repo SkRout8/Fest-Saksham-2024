ccc0b817f954a8b0f20e29530c718a58e91cd1aa
Shashikanta Rout <116004580+SkRout8@users.noreply.github.com>
2024-02-23 11:05:18 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -33,10 +33,12 @@ const Home = ({ isLoggedIn }) => {
               SAKSHAM-2K24
             </h1>
 
-            <p className=' text1 text-white text-center text-2xl'>Welcome to the Technical fest of Srinix College of Engineering,Balasore
+            <p className=' text1 text-white  text-2xl'>Welcome to the Technical fest of Srinix College of Engineering,Balasore
             </p>
-            <button className='btn1  mt-4 text-white w-40 h-10 text-center   bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ... rounded 	  hover: animate-bounce	 	'>
-              Get Started
+            <button className='btn1  mt-4 text-white w-40 h-10 text-center   bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ... rounded 	  hover: animate-bounce'>
+               <div className='  text-center '>
+               Get Started
+               </div>
             </button>
           </div>
 
@@ -58,10 +60,10 @@ const Home = ({ isLoggedIn }) => {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                 <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
               </svg>
-              <span className="ml-3 text-xl">Tailblocks</span>
+              <span className="ml-3 text-xl">SAKSHAM</span>
             </a>
-            <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2020 Tailblocks —
-              <a href="https://twitter.com/knyttneve" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">@knyttneve</a>
+            <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2024 SAKSHAM —
+              <a href="https://twitter.com/knyttneve" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer"></a>
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
               <a className="text-gray-400">
@@ -74,7 +76,7 @@ const Home = ({ isLoggedIn }) => {
                   <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                 </svg>
               </a>
-              <a className="ml-3 text-gray-400">
+              <a className="ml-3 text-gray-400" href=' https://www.instagram.com/saksham_sce?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=='>
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
                   <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                   <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
